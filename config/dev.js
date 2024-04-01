091e12bc4822250821371cc9ec318c6b5e1764f5
aviya-ha <aviyaharel6@gmail.com>
2024-04-02 02:07:02 +0300

    Debug

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default  {
   // dbURL: 'mongodb://127.0.0.1:27017',
-  dbURL: 'mongodb+srv://aviya:aviya123@cluster0.gktvyef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbURL: 'mongodb+srv://aviya:1234@cluster0.gktvyef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : 'toyDB'
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  // dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://aviya:1234@cluster0.gktvyef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'toyDB'
 }
